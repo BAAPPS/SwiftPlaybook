e58bf023f4b8338168f63dd4c476d9b300bb46fa
BAAPPS <208905508+BAAPPS@users.noreply.github.com>
2026-06-12 10:52:10 -0700

    Fix: remove submodule and convert to normal folder

---
+++ 01-API-Networking/01-API-Networking/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  01-API-Networking
+//
+//  Created by D F on 6/12/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 01-API-Networking/01-API-Networking/_1_API_NetworkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  _1_API_NetworkingApp.swift
+//  01-API-Networking
+//
+//  Created by D F on 6/12/26.
+//
+
+import SwiftUI
+
+@main
+struct _1_API_NetworkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
