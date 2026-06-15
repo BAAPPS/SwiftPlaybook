67ab3f681b5e06bc49704d8899394696ba079e69
BAAPPS <208905508+BAAPPS@users.noreply.github.com>
2026-06-15 14:39:28 -0700

    feat(news_api  networking): news API key intial commit

---
+++ 02-Networking-NewsAPI/02-Networking-NewsAPI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  02-Networking-NewsAPI
+//
+//  Created by D F on 6/12/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 02-Networking-NewsAPI/02-Networking-NewsAPI/_2_Networking_NewsAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  _2_Networking_NewsAPIApp.swift
+//  02-Networking-NewsAPI
+//
+//  Created by D F on 6/12/26.
+//
+
+import SwiftUI
+
+@main
+struct _2_Networking_NewsAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
